cd7ecdd587e86397820bdaa7f06f3d921dad6b1b
Dylan Piette <44063839+Kamigoro@users.noreply.github.com>
2020-09-30 22:03:05 +0200

    Adding a map with mapbox

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ProjectList from './components/ProjectList';
+import Map from './components/Map';
 
 
 ReactDOM.render(<ProjectList/>, document.getElementById('projectList'));
+ReactDOM.render(<Map />, document.getElementById('map'));
 
