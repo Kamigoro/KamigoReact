cb58719cb6da38229492fa57dc041599ad937e3e
Dylan Piette <44063839+Kamigoro@users.noreply.github.com>
2020-10-01 09:31:02 +0200

    removing useless component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ProjectList from './components/ProjectList';
-import Map from './components/Map';
 
 
 ReactDOM.render(<ProjectList/>, document.getElementById('projectList'));
-ReactDOM.render(<Map />, document.getElementById('map'));
 
